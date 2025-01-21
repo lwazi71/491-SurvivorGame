e7eb7b031523485115616f9661565cee56dc385b
nolimitz71 <greninja71@gmail.com>
2025-01-20 21:32:13 -0800

    Dungeon class body

---
+++ Dungeon.js
@@ -0,0 +1,22 @@
+class Dungeon{
+    constructor(width,height,spriteSheet){
+        this.width = width;
+        this.height = height;
+        this.spriteSheet = spriteSheet;
+        this.grid = []; // 2d array for dungeon layout
+        this.rooms = []; // Array of room objects
+
+        this.generarteGrid(); // Generates the dungeon
+    }
+
+    generarteGrid(){ // Generates
+    }
+
+    generarteMaze(){
+
+    }
+
+    printDungeon(){ // Print
+    }
+    
+}
