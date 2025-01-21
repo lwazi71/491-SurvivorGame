e3c29b80dfcd02f02d5a17f9bbab6b7467cca2e4
nolimitz71 <greninja71@gmail.com>
2025-01-20 19:23:27 -0800

    Added dungeon sprites disabled animations

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new GreenGhoul(gameEngine));
+	//gameEngine.addEntity(new GreenGhoul(gameEngine));
 
 	gameEngine.init(ctx);
 
